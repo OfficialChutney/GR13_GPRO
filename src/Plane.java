db00f93b8c7252c1fd0e8026ee90de09cc280928
Noah8282 <nova0001@stud.kea.dk>
2024-11-21 16:25:14 +0100

    FileReader made

--- src/Plane.java
+++ src/Plane.java
@@ -7,15 +7,26 @@ public class Plane {
 
     Program program;
     World world;
-    int worldSize;
-    HashMap<String, String> initialConditions;
+    int displaySize;
+    int delay;
 
 
     public Plane() {
+        displaySize = 800;
+        delay = 1000;
+    }
 
+    public void start(int worldSize, HashMap<String, String> initialConditions) {
+        program = new Program(worldSize, displaySize, delay);
+        world = program.getWorld();
+
+        program.show();
+
+        for (int i = 0; i < 20; i++) {
+            program.simulate();
+        }
 
 
     }
-
 
 }
