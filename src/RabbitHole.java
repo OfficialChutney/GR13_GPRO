52b0eb992337d2ae46a67ead9eb7a08c23b4016e
Noah8282 <nova0001@stud.kea.dk>
2024-11-21 20:25:24 +0100

    Made rabbit tunnels

--- src/RabbitHole.java
+++ src/RabbitHole.java
@@ -4,18 +4,24 @@ import itumulator.world.World;
 import itumulator.simulator.Actor;
 
 import java.util.ArrayList;
-import java.util.Set;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Random;
 
 
 public class RabbitHole implements NonBlocking, Actor {
 
-    private final World ourWorld;
-    private final Location tileLocation;
+    private World ourWorld;
+    private final Location mainLocation;
+    private HashSet<RabbitHole> entrances;
     private boolean hasRabbit;
+    private float chanceOfEntrance = 0.1f;
 
     RabbitHole(World ourWorld, Location tileLocation) {
         this.ourWorld = ourWorld;
-        this.tileLocation = tileLocation;
+        this.mainLocation = tileLocation;
+        entrances = new HashSet<>();
+        createEntrances();
     }
 
     @Override
@@ -32,8 +38,46 @@ public class RabbitHole implements NonBlocking, Actor {
         return hasRabbit;
     }
 
-    public Location getTileLocation() {
-        return tileLocation;
+    public Location getMainLocation() {
+        return mainLocation;
+    }
+
+    private void createEntrances() {
+        Random rd = new Random();
+        if (rd.nextFloat(1) < chanceOfEntrance) {
+            Map<Object, Location> allEntities = ourWorld.getEntities();
+            ArrayList<RabbitHole> rabbitHoles = new ArrayList<>();
+            for (Object currentObject : allEntities.keySet()) {
+                if (currentObject instanceof RabbitHole x && currentObject != this) {
+                    rabbitHoles.add(x);
+                }
+            }
+
+            if (!rabbitHoles.isEmpty()) {
+
+
+                int indexToConnect = rd.nextInt(rabbitHoles.size());
+                RabbitHole holeToConnect = rabbitHoles.get(indexToConnect);
+                holeToConnect.addToEntrance(this);
+                entrances.add(holeToConnect);
+                rabbitHoles.remove(holeToConnect);
+
+                for (RabbitHole otherHoles : rabbitHoles) {
+                    if (rd.nextInt(5) == 1) {
+                        entrances.add(otherHoles);
+                        otherHoles.addToEntrance(this);
+                    }
+                }
+
+            }
+        }
+
+        System.out.println(entrances.toString());
+    }
+
+    public void addToEntrance(RabbitHole rabbitHole) {
+        entrances.add(rabbitHole);
+        System.out.println(entrances.toString());
     }
 
 
