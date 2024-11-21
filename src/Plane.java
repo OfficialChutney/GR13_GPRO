8eb718cc2124768c643e2c69961f8eeabbccef53
Noah8282 <nova0001@stud.kea.dk>
2024-11-21 16:15:39 +0100

    FileReader made

--- src/Plane.java
+++ src/Plane.java
@@ -1,2 +1,21 @@
+import itumulator.executable.Program;
+import itumulator.world.World;
+
+import java.util.HashMap;
+
 public class Plane {
+
+    Program program;
+    World world;
+    int worldSize;
+    HashMap<String, String> initialConditions;
+
+
+    public Plane() {
+
+
+
+    }
+
+
 }
