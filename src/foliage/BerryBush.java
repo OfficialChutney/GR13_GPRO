ebed8cf6e01b4ea4b660909fdf0c30eb828df743
Philip Quorning <phqu@itu.dk>
2024-12-05 12:48:53 +0100

    im Commiting

--- src/foliage/BerryBush.java
+++ src/foliage/BerryBush.java
@@ -10,6 +10,9 @@ import itumulator.world.World;
 import java.awt.*;
 import java.util.Random;
 
+/**
+ * BerryBush er klassen som styre adfæret af buskene i simulationen.
+ */
 public class BerryBush implements Actor, NonBlocking, DynamicDisplayInformationProvider {
     private Location tileLocation;
     private boolean hasBerries = true;
@@ -18,6 +21,10 @@ public class BerryBush implements Actor, NonBlocking, DynamicDisplayInformationP
         this.tileLocation = this.getTileLocation();
     }
 
+    /**
+     * act tjekker om busken har bær, hvis den ikke har kalder den chanceToGrowBerries.
+     * @param world providing details of the position on which the actor is currently located and much more.
+     */
     @Override
     public void act(World world) {
         if (!hasBerries) {
@@ -25,24 +32,43 @@ public class BerryBush implements Actor, NonBlocking, DynamicDisplayInformationP
         }
     }
 
+    /**
+     * chanceToGrowBerries har en chance for at sætte hasBerries til True.
+     */
     private void chanceToGrowBerries() {
         Random rand = new Random();
         if (!hasBerries && rand.nextInt(8) > 5) {
             hasBerries = true;
         }
     }
+
+    /**
+     * retunere buskens Location.
+     * @return Location
+     */
     public Location getTileLocation() {
         return tileLocation;
     }
 
+    /**
+     * eatBerries sætter hasBerries til False.
+     */
     public void eatBerries(){
         hasBerries = false;
     }
 
+    /**
+     * BerryState retunere hasBerries
+     * @return Boolean
+     */
     public boolean BerryState() {
         return hasBerries;
     }
 
+    /**
+     * getInformation retunere den nødvendige DisplayInformation ud fra busken status.
+     * @return DisplayInformation
+     */
     @Override
     public DisplayInformation getInformation() {
         if(hasBerries) {
