ac71ff222484bd616c601698feb51b320de59ccc
Philip Quorning <phqu@itu.dk>
2024-12-09 10:51:06 +0100

    im Commiting

--- src/animal/Bear.java
+++ src/animal/Bear.java
@@ -24,6 +24,7 @@ public class Bear extends Animal {
     protected ArrayList<Location> territoryTileList;
     protected BearBehavior bearBehavior;
     protected Actor bearTarget;
+    protected Location centerOfTerritory;
 
 
     public Bear(World world) {
@@ -87,13 +88,14 @@ public class Bear extends Animal {
      * @param loc
      */
     protected void setTerritory(Location loc) {
-        int tSize = 2;
+        int tSize = 3;
         int startX = loc.getX();
         int startY = loc.getY();
         territoryTopLeftCornor = new Location(startX - tSize, startY - tSize);
         territoryLowerRightCornor = new Location(startX + tSize, startY + tSize);
 
         territoryTileList = new ArrayList<>(world.getSurroundingTiles(tSize));
+        centerOfTerritory = world.getLocation(this);
     }
 
     /**
@@ -192,13 +194,21 @@ public class Bear extends Animal {
     protected void normalBehavior() {
         if (checktime() == TimeOfDay.MORNING) {
             status = AnimalStatus.LOOKINGFORFOOD;
-            pathFinder(getNearestBearFood());
+            if(!haveILeftHome()) {
+                pathFinder(getNearestBearFood());
+            } else {
+                pathFinder(centerOfTerritory);
+            }
             eat();
             updateEnergy(-1);
 
         } else if (checktime() == TimeOfDay.EVENING) {
             status = AnimalStatus.LOOKINGFORFOOD;
-            pathFinder(getNearestBearFood());
+            if(!haveILeftHome()) {
+                pathFinder(getNearestBearFood());
+            } else {
+                pathFinder(centerOfTerritory);
+            }
             eat();
             updateEnergy(-1);
 
@@ -330,5 +340,18 @@ public class Bear extends Animal {
      */
     public void setBearBehavior(BearBehavior bearBehavior) {
         this.bearBehavior = bearBehavior;
+    }
+
+    protected boolean haveILeftHome(){
+        for (int i = 0; i < territoryTileList.size(); i++) {
+            Location temp = territoryTileList.get(i);
+            if (!world.isTileEmpty(temp)) {
+                if (!(world.getTile(temp) == this)) {
+                    System.out.println("I have Left Home");
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
